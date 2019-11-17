5692b053147185aee916e5ab8f88f9afeca7404a
tardis2snej <nadya.snezhik@gmail.com>
2019-11-17 13:05:13 +0200

    Добавлены класы пугателей

---
+++ Helpmare/Assets/Scripts/ScaryThings.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ScaryThings : MonoBehaviour
+{
+    [SerializeField]
+    private Transform dreamer;
+    [SerializeField]
+    private AudioSource _audio;
+    private float rangeRadius;
+
+    [SerializeField]
+    private AudioClip[] noises;
+    [SerializeField]
+    private float maxDamage = 40f;
+    [SerializeField]
+    private AnimationCurve powerOfDamage;
+    [SerializeField]
+    private float displayTime = 5f;
+
+    private void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+        rangeRadius = GetComponent<BoxCollider2D>().size.x / 2;
+        noises = new AudioClip[1];
+    }
+
+    public void Scare()
+    {
+        float distanceToDreamer = Vector2.Distance(transform.position, dreamer.position);
+        if (!CanScare(distanceToDreamer))
+            return;
+
+        //Debug.Log("StressUp by " + maxDamage * powerOfDamage.Evaluate(1 - (distanceToDreamer / rangeRadius)) + "Evaluated: " + powerOfDamage.Evaluate(1 - (distanceToDreamer / rangeRadius)));
+        Stress.Instance.StressUp(maxDamage * powerOfDamage.Evaluate(1 - (distanceToDreamer / rangeRadius)));
+
+        SelectAndPlayAudio();
+        DisplayGraphycs();
+
+        StartCoroutine("Hide");
+    }
+
+    private bool CanScare(float distance)
+    {
+        return distance <= rangeRadius;
+    }
+
+    private void SelectAndPlayAudio()
+    {
+        _audio.Stop();
+        _audio.clip = noises[Random.Range(0, noises.Length)];
+        _audio.Play();
+    }
+
+    private void DisplayGraphycs()
+    {
+        //TO DO
+    }
+
+
+    public IEnumerator Hide()
+    {
+        yield return new WaitForSeconds(displayTime);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Helpmare/Assets/temp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temp : MonoBehaviour
+{
+    public GameObject sc;
+    private bool sc_prev_flag = false;
+    public bool sc_flag = false;
+    
+    void Update()
+    {
+        if (sc_flag != sc_prev_flag)
+        {
+            //sc.SetActive(!sc.activeInHierarchy);
+            sc.GetComponent<ScaryNoise>().Scare();
+            sc_prev_flag = sc_flag;
+        }
+    }
+}
